93fe9c10b6c967c5fb9e21e9e3496ae94876446e
Doug Chestnut <dougchestnut@gmail.com>
2015-11-03 14:26:14 -0500

    added html links for descriptions

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ var getFacebookSource = function(sources, callback){
                       (item.status_type=="added_video")?
                         item.from.name+" shared a video on Facebook":
                         item.from.name+" shared on Facebook",
-            description:((item.message)? item.message+"<br />":"") +
-                        ((item.description)? item.description:""),
+            description:((item.message)? item.message.parseURL().parseUsername().parseHashtag() +"<br />":"") +
+                        ((item.description)? item.description.parseURL().parseUsername().parseHashtag() :""),
             url:(item.link)? item.link: null,
             categories:['facebook',item.type],
             author:"https://www.facebook.com/"+item.from.id+"/",
@@ -72,7 +72,7 @@ var getTwitterSource = function(sources, callback){
         if (!item.in_reply_to_status_id && !item.possibly_sensitive) {
           items.push({
               title:item.user.screen_name+" tweeted",
-              description:item.text,
+              description: item.text.parseURL().parseUsername().parseHashtag() ,
               url:"https://twitter.com/UVaLibrary/status/"+item.id_str,
               categories:item.entities.hashtags,
               author:"http://twiter.com/"+item.user.screen_name,
@@ -97,3 +97,22 @@ var getRssSource = function(sources, callback){
   callback( feed.xml() ); 
 };
 
+String.prototype.parseURL = function() {
+	return this.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&~\?\/.=]+/g, function(url) {
+		return url.link(url);
+	});
+};
+
+String.prototype.parseUsername = function() {
+	return this.replace(/[@]+[A-Za-z0-9-_]+/g, function(u) {
+		var username = u.replace("@","")
+		return u.link("http://twitter.com/"+username);
+	});
+};
+
+String.prototype.parseHashtag = function() {
+	return this.replace(/[#]+[A-Za-z0-9-_]+/g, function(t) {
+		var tag = t.replace("#","%23")
+		return t.link("http://search.twitter.com/search?q="+tag);
+	});
+};
